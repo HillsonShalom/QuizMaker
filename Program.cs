18cd7eaa667246de388da7f0075e132f109a7e90
Hillson <hillsonshalom@gmail.com>
2024-07-21 12:31:37 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace QuizMaker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine(@"1. Create questions
+2. Answer Questions
+");
+            string input = Console.ReadLine();
+            if (input == "1")
+            {
+                XMLService xmlService = new XMLService("Data/data.xml");
+                Console.WriteLine("-Type your question");
+                string ques = Console.ReadLine();
+                Console.WriteLine("-Type the answer");
+                string ans = Console.ReadLine();
+                Quiz newQuiz = new Quiz(ques, ans);
+                xmlService.CreateNodeFromModel(newQuiz);
+            }else if (input == "2")
+            {
+                XMLService xMLService = new XMLService("Data.data.xml");
+                
+            }
+        }
+    }
+
+    public class Quiz
+    {
+        public string question;
+        public string answer;
+
+        public Quiz(string question, string answer)
+        {
+            this.question = question;
+            this.answer = answer;
+        }
+
+        public Quiz() { }
+    }
+}
